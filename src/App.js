3fe0aada45407e6a2f259eea1d5e6abad09b3d16
Jay Patel <jaypatel120847@gmail.com>
2021-12-01 11:15:30 +0530

    Remove important from all css files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Layout from "./component/Layouts/DashboardLayout/Index";
-import Cards from "./component/StackingCard";
 function App() {
   return (
     <div className="App">
       <Layout />
-      <Cards />
     </div>
   );
 }
