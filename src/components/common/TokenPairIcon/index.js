ba4dfe4946386524c6ede14d6d3b880cd7b7f0bc
Kunal Dhaibar <kunal@aliteprojects.in>
2021-12-03 12:14:08 +0530

    Fixed token pair component

--- src/components/common/TokenPairIcon/index.js
+++ src/components/common/TokenPairIcon/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styles from "./TokenPairIcon.module..css";
+import styles from "./TokenPairIcon.module.css";
 
 const TokenPairIcon = ({ image1, image2 }) => {
   return (
