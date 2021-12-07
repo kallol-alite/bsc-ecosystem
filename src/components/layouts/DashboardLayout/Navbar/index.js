171031c1dc0612ecd21ac6ce85ccb7c229312cdf
Kunal Dhaibar <kunal@aliteprojects.in>
2021-12-07 13:07:43 +0530

    Network card changes

--- src/components/layouts/DashboardLayout/Navbar/index.js
+++ src/components/layouts/DashboardLayout/Navbar/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 import styles from "./Navbar.module.css";
 import Logo from "../../../../assets/logo.svg";
 
 import { setSidebarView } from "../../../../actions/master-actions";
 import ConnectWalletButton from "../../../ConnectWalletButton";
-import { useHistory } from "react-router-dom";
-import NetworkSwitch from "../../../modals/NetworkSwitch";
+// import NetworkSwitch from '../../../NetworkSwitch';
 
 const Navbar = () => {
   const dispatch = useDispatch();
@@ -20,21 +20,12 @@ const Navbar = () => {
       <div className={styles.burgerIconDiv}>
         <i className={`bi ${isSidebarOpen ? "bi-x" : "bi-list"} fs-3 p-1`} onClick={() => dispatch(setSidebarView(!isSidebarOpen))}></i>
       </div>
-      <div
-        className={styles.logoDiv}
-        onClick={() => {
-          history.push("/");
-        }}
-      >
+      <div className={styles.logoDiv} onClick={() => {history.push('/')}}>
         <img src={Logo} />
       </div>
       <div className={styles.linkElements + " mx-2"}>
-        {/* need to refactor for smaller screens */}
         <ConnectWalletButton />
-      </div>
-      <div className="mx-2">
-        {" "}
-        <NetworkSwitch />
+        {/* <NetworkSwitch /> */}
       </div>
     </div>
   );
