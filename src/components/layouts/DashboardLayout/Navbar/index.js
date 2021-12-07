2df0bda672fde8c0e1d6a8df9156e2820b4e4abf
Jay Patel <jaypatel120847@gmail.com>
2021-12-07 12:24:55 +0530

    Added Network Switch

--- src/components/layouts/DashboardLayout/Navbar/index.js
+++ src/components/layouts/DashboardLayout/Navbar/index.js
@@ -7,6 +7,7 @@ import Logo from "../../../../assets/logo.svg";
 import { setSidebarView } from "../../../../actions/master-actions";
 import ConnectWalletButton from "../../../ConnectWalletButton";
 import { useHistory } from "react-router-dom";
+import NetworkSwitch from "../../../modals/NetworkSwitch";
 
 const Navbar = (props, context) => {
   const dispatch = useDispatch();
@@ -19,12 +20,21 @@ const Navbar = (props, context) => {
       <div className={styles.burgerIconDiv}>
         <i className={`bi ${isSidebarOpen ? "bi-x" : "bi-list"} fs-3 p-1`} onClick={() => dispatch(setSidebarView(!isSidebarOpen))}></i>
       </div>
-      <div className={styles.logoDiv} onClick={() => {history.push('/')}}>
+      <div
+        className={styles.logoDiv}
+        onClick={() => {
+          history.push("/");
+        }}
+      >
         <img src={Logo} />
       </div>
       <div className="mx-2">
         {/* need to refactor for smaller screens */}
         <ConnectWalletButton />
+      </div>
+      <div className="mx-2">
+        {" "}
+        <NetworkSwitch />
       </div>
     </div>
   );
